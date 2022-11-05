b97e15f0c640a79dc242922d199b0cb488ef0dfe
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-05 21:47:24 +0530

    Multiply

---
+++ Multiplication/Multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    internal class Multiplication
+    {
+        public void Multiply(int a, int b, int c)
+        {
+            int result = a * b * c; 
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Multiplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Multiplication multiplication = new Multiplication();
+            multiplication.Multiply(20, 30, 60);
+
+            // Addition.Add();
+            //int a, b;
+            //Console.WriteLine();
+
+        }
+    }
+}
